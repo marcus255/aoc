947fc9b7c3f5c4226cd12380815a914bf469496e
marcus255 <marek255trojan@gmail.com>
2023-12-18 13:27:01 +0100

    Try numpy algorithm for task 16 p 2

--- 2023/task_18.py
+++ 2023/task_18.py
@@ -23,6 +23,8 @@ coords = []
 max_r, min_r, max_c, min_c = pos[0], pos[1], pos[0], pos[1]
 for move, depth in zip(moves, depths):
     m = dirs[move]
+    
+    coords.append((pos, move))
     for _ in range(depth):
         max_r = pos[0] if pos[0] > max_r else max_r
         min_r = pos[0] if pos[0] < min_r else min_r
@@ -30,7 +32,7 @@ for move, depth in zip(moves, depths):
         min_c = pos[1] if pos[1] < min_c else min_c
         pos = (pos[0] + m[0], pos[1] + m[1])
         grid[pos[0]][pos[1]] = '#'
-        coords.append((pos, move))
+    prev_move = move
 
 for r in range(min_r, max_r+1):
     row = grid[r]
@@ -38,28 +40,43 @@ for r in range(min_r, max_r+1):
     # if r > max_r:
     #     break
 
-prev = (0, 0)
-prev_move = (0, 0)
-for (r, c), m in coords:
-    inside = inside_moves[m]
-    # print(r, c, inside)
-    if inside == DOWN or prev == DOWN or (prev_move == DOWN and m == RIGHT):
-        for rr in range(r + 1, SIZE[0]):
-            if grid[rr][c] == '#':
-                # print(f'break at {rr}, {c}')
-                break
-            else:
-                grid[rr][c] = '#'
-    if inside == UP or prev == UP:
-        for rr in range(r - 1, 0, -1):
-            if grid[rr][c] == '#':
-                # print(f'break at {rr}, {c}')
-                break
-            else:
-                grid[rr][c] = '#'
-    prev = inside
-    prev_move = m
+# prev = (0, 0)
+# prev_move = (0, 0)
+# for (r, c), m in coords:
+#     inside = inside_moves[m]
+#     # print(r, c, inside)
+#     if inside == DOWN or prev == DOWN or (prev_move == DOWN and m == RIGHT):
+#         for rr in range(r + 1, SIZE[0]):
+#             if grid[rr][c] == '#':
+#                 # print(f'break at {rr}, {c}')
+#                 break
+#             else:
+#                 grid[rr][c] = '#'
+#     if inside == UP or prev == UP:
+#         for rr in range(r - 1, 0, -1):
+#             if grid[rr][c] == '#':
+#                 # print(f'break at {rr}, {c}')
+#                 break
+#             else:
+#                 grid[rr][c] = '#'
+#     prev = inside
+#     prev_move = m
         
+
+import numpy as np
+rrr = [r for (r, c), _ in coords]
+ccc = [c for (r, c), _ in coords]
+print(rrr)
+print(ccc)
+
+def PolyArea(x,y):
+    return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+area = PolyArea(rrr, ccc)
+print(f'area: {area}')
+area = int(area)
+# 0.26353377782163534
+
+
 print()
 print()
 print()
@@ -70,14 +87,15 @@ for r in range(min_r, min_r + 15): #, max_r+1):
     # if r > max_r:
     #     break
 
-result1 = 0
+path_len = 0
 for r, _ in enumerate(grid):
     for c, _ in enumerate(row):
         if grid[r][c] == '#':
-            result1 += 1
+            path_len += 1
 print(min_r, max_r, min_c, max_c)
 print(max_r - min_r, max_c - min_c)
 
+result1 = path_len + area
 aoc.print_result(1, result1, exp1)
 
 # Part 2
