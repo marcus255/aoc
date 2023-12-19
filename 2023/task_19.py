2382201dc4b9a9e9de981a59a0c33f9f0f0ad784
Marek Trojan <marek255trojan@gmail.com>
2023-12-19 20:34:22 +0100

    Cleanup task 19

--- 2023/task_19.py
+++ 2023/task_19.py
@@ -2,9 +2,11 @@ import aoc
 
 lines = aoc.get_lines(__file__)
 result1, result2 = 0, 0
-exp1, exp2 = 1, 1
+exp1, exp2 = 19114, 167409079868000
 
 # Common
+STARTING_WORKFLOW = 'in'
+REJECTED, ACCEPTED = 'R', 'A'
 workflows = {}
 for line in lines:
     if line == '':
@@ -14,8 +16,8 @@ for line in lines:
     w = []
     for r in rules:
         if ':' in r:
-            cond, target = r.split(':')
-            w.append((cond[0], cond[1], int(cond[2:]), target))
+            op, target = r.split(':')
+            w.append((op[0], op[1], int(op[2:]), target))
         else:
             w.append((None, None, None, r))
     workflows[w_name] = w
@@ -25,187 +27,92 @@ ratings = []
 for line in lines[len(workflows)+1:]:
     ratings_in_line = line[1:-1].split(',')
     r = {}
-    for rat in ratings_in_line:
-        name, number = rat.split('=')
+    for rating in ratings_in_line:
+        name, number = rating.split('=')
         r[name] = int(number)
     ratings.append(r)
     # print(r)
 
 # Part 1
 aoc.mark_task_start()
-accepted = 0
-count = 0
-print()
-for rat in ratings:
+total_rating = 0
+for rating in ratings:
     done = False
-    cur_workf = 'in'
-    # print(f'Rating: {rat}')
+    curr_workflow = 'in'
     while not done:
-        # print(f'  cur_workf: {cur_workf}')
-        if cur_workf == 'A':
-            accepted += 1
-            # print(f'Accepted: ', rat)
-            count += sum(rat.values())
+        if curr_workflow == REJECTED:
             break
-        elif cur_workf == 'R':
+        elif curr_workflow == ACCEPTED:
+            total_rating += sum(rating.values())
             break
-        conditions = workflows[cur_workf]
-        for x, cond, num, next in conditions:
-            # print(f'    ', x, cond, num, next)
-            if x == None:
-                if next in ['A', 'R']:
-                    if next == 'A':
-                        # print(f'Accepted: ', rat)
-                        accepted += 1
-                        count += sum(rat.values())
+        conditions = workflows[curr_workflow]
+        for cat, op, number, next_workflow in conditions:
+            if cat == None:
+                if next_workflow == REJECTED:
                     done = True
-                    break
+                elif next_workflow == ACCEPTED:
+                    total_rating += sum(rating.values())
+                    done = True
                 else:
-                    cur_workf = next
+                    curr_workflow = next_workflow
                     break
-            else:
-                cond_true = False
-                if cond == '<':
-                    if rat[x] < num:
-                        cond_true = True
-                if cond == '>':
-                    if rat[x] > num:
-                        cond_true = True
-                if cond_true:
-                    cur_workf = next
-                    break
+            elif op == '<' and rating[cat] < number or op == '>'and rating[cat] > number:
+                curr_workflow = next_workflow
+                break
 
-print(accepted)
-print(count)
-
-result1 = count
+result1 = total_rating
 aoc.print_result(1, result1, exp1)
 
 # Part 2
 aoc.mark_task_start()
-# accepted = 0
-# count = 0
 MAX_NUM = 4000
-# for rat in ratings:
-#     done = False
-#     cur_workf = 'in'
-#     # print(f'Rating: {rat}')
-#     while not done:
+CATEGORIES = 'asxm'
+total_combinations = 0
 
+def compute_combinations(ranges):
+    # print(f'Possible ranges: {ranges}')
+    combinations = 1
+    for category in 'asxm':
+        set_of_ranges = [set(r) for cat, r in ranges if cat == category]
+        # if there is no ranges for given category, it means it can have all values from <1-4000>
+        combinations *= len(set.intersection(*set_of_ranges)) if set_of_ranges else MAX_NUM
+    return combinations
 
-all_ranges = []
-grand_total = 0
-def do_stuff(workf_list=None, next_line_workf=None, ranges=None):
-    # print(f'  cur_workf: {cur_workf}')
-    global all_ranges
-    global grand_total
-    if ranges is None:
-        ranges = []
-    rrr = 0
-    next_line = None
-    if next_line_workf:
-        if next_line_workf == 'A':
-            print(f'   >>> R is {ranges}')
-            total = 1
-            for ch in 'asxm':
-                ss = [set(x) for c, x in ranges if c == ch]
-                if ss:
-                    s = set.intersection(*ss)
-                    total *= len(s)
-                    print(f'     len is {len(s)}')
-                else:
-                    total *= MAX_NUM
-            print(f'        Total: {total}\n')
-            grand_total += total
+def process_workflows(ranges, workflow_rules, workflow):
+    global total_combinations
 
-            all_ranges += ranges[:]
-            return rrr
-        elif next_line_workf == 'R':
-            return 0
-        next_line = workflows[next_line_workf]
-    else:
-        next_line = workf_list
+    if workflow == ACCEPTED:
+        total_combinations += compute_combinations(ranges)
+        return
+    elif workflow == REJECTED:
+        return
     
-    # print(f'Next line: {next_line}')
-    x, cond, num, cur_workf = next_line[0]
+    next_line = workflows[workflow] if workflow else workflow_rules
+    first_rule, remaining_rules = next_line[0], next_line[1:]
+    cat, op, number, next_workflow = first_rule
 
-    if x == None:
-        if cur_workf == 'A':
-            print(f'   >>> R is {ranges}')
-            total = 1
-            for ch in 'asxm':
-                ss = [set(x) for c, x in ranges if c == ch]
-                if ss:
-                    s = set.intersection(*ss)
-                    total *= len(s)
-                    print(f'     len is {len(s)}')
-                else:
-                    total *= MAX_NUM
-            print(f'        Total: {total}\n')
-            grand_total += total
+    if cat == None:
+        if next_workflow == ACCEPTED:
+            total_combinations += compute_combinations(ranges)
+        elif next_workflow != REJECTED:
+            process_workflows(ranges, None, next_workflow)
+        return
 
-            all_ranges += ranges[:]
-            return rrr
-        elif cur_workf == 'R':
-            return 0
-        else:
-            return do_stuff(None, next_line_workf=cur_workf, ranges=ranges)
+    if op == '<':
+        # Example: a<2006  =>  range <1-2005> if condition met, <2006-4000> if not
+        r1, r2 = range(1, number), range(number, MAX_NUM + 1)
+        # Branch for condition met - pass name of next workflow
+        process_workflows(ranges + [(cat, r1)], None, next_workflow)
+        # Branch for condition not met - pass remaining rules from current line
+        process_workflows(ranges + [(cat, r2)], remaining_rules, None)
+    elif op == '>':
+        # Example: x>2662  =>  range <2663-4000> if condition met, <1-2662> if not
+        r1, r2 = range(number + 1, MAX_NUM + 1), range(1, number + 1)
+        # Branch for condition met - pass name of next workflow
+        process_workflows(ranges + [(cat, r1)], None, next_workflow)
+        # Branch for condition not met - pass remaining rules from current line
+        process_workflows(ranges + [(cat, r2)], remaining_rules, None)
 
-    if cond == '<':
-        # a<2006 ---> 1-2005
-        r1 = (range(1, num))
-        new_ranges = ranges[:]
-        new_ranges.append((x, r1))
-        rrr += do_stuff(next_line_workf, cur_workf, ranges=new_ranges)
-
-        # not a<2006 ---> 2006-4000
-        r2 = (range(num, MAX_NUM+1))
-        new_ranges = ranges[:]
-        new_ranges.append((x, r2))
-        # print(f'< range: {r1} {r2}')
-        rrr += do_stuff(workf_list=next_line[1:], next_line_workf=None, ranges=new_ranges)
-
-    if cond == '>':
-        # x>2662 ---> 2663-4000
-        r1 = (range(num+1, MAX_NUM+1))
-        new_ranges = ranges[:]
-        new_ranges.append((x, r1))
-        rrr += do_stuff(next_line_workf, cur_workf, ranges=new_ranges)
-        
-        # not x>2662 ---> 1-2662
-        r2 = (range(1, num+1))
-        new_ranges = ranges[:]
-        new_ranges.append((x, r2))
-        rrr += do_stuff(workf_list=next_line[1:], next_line_workf=None, ranges=new_ranges)
-        # print(f'> range: {r1} {r2}')
-    return rrr
-
-rrr = do_stuff(next_line_workf='in')
-print(rrr)
-print(grand_total)
-# for ch in 'asxm':
-#     sets_for_ch = [x for c, x in all_ranges if c == ch]
-#     print(f'sets for {ch}: {sets_for_ch}')
-#     s = set.intersection(*[set(x) for c, x in all_ranges if c == ch])
-#     print(f'len for {ch} is {len(s)}')
-
+process_workflows([], None, 'in')
+result2 = total_combinations
 aoc.print_result(2, result2, exp2)
-'''
-px{a<2006:qkq,m>2090:A,rfg}
-pv{a>1716:R,A}
-lnx{m>1548:A,A}
-rfg{s<537:gd,x>2440:R,A}
-qs{s>3448:A,lnx}
-qkq{x<1416:A,crn}
-crn{x>2662:A,R}
-in{s<1351:px,qqz}
-qqz{s>2770:qs,m<1801:hdj,R}
-gd{a>3333:R,R}
-hdj{m>838:A,pv}
-
-{x=787,m=2655,a=1222,s=2876}
-{x=1679,m=44,a=2067,s=496}
-{x=2036,m=264,a=79,s=2244}
-{x=2461,m=1339,a=466,s=291}
-{x=2127,m=1623,a=2188,s=1013}
-'''
