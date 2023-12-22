d8f4091a970daf9b472c2af67eafbec58acb9b30
marcus255 <marek255trojan@gmail.com>
2023-12-22 18:18:00 +0100

    Task 22 part 2

--- 2023/task_22.py
+++ 2023/task_22.py
@@ -51,89 +51,109 @@ def movable(block, grid):
     return False
 
 def move_down(block, grid):
-
     move_illegal = False
-    moved = False
     while not move_illegal:
         new_pos = [[x, y, z-1] for x, y, z in block]
-        for i, (x, y, z) in enumerate(new_pos):
-            # if block == [[1, 1, 5], [1, 1, 6]] and [0, 1, 4] not in grid:
-            #     import pdb
-            #     pdb.set_trace()
+        for x, y, z in new_pos:
             if z < 1 or [x, y, z] in grid:
-                # print(f'Move illegal: {i}: {x},{y},{z}')
                 move_illegal = True
                 break
         if not move_illegal:
             block[:] = copy.deepcopy(new_pos)
-            moved = True
     grid += block
-    return moved
 
+def print_blocks(blocks, width=12, height=20):
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    line = ['.' for _ in range(width)]
+    grid = [line[:] for _ in range(height)]
+    grid2 = [line[:] for _ in range(height)]
+
+    for i, block in enumerate(blocks):
+        for x, y, z in block:
+            grid[height-z][x] = letters[i%len(letters)]
+            grid2[height-z][y] = letters[i%len(letters)]
+    
+    for l1, l2 in zip(grid, grid2):
+        print(f'{"".join(l1)}  {"".join(l2)}')
+
+def move_all_down(grid, blocks):
+    print(f'blocks:\n{blocks}')
+    blocks = sorted(blocks, key=lambda x:x[0][Z])
+    print(f'Sorted blocks:\n{blocks}')
+
+    for block in blocks:
+        move_down(block, grid)
 
 # Part 1
 aoc.mark_task_start()
 grid = []
-
-print(f'blocks:\n{blocks}')
-blocks = sorted(blocks, key=lambda x:x[0][Z])
-print(f'Sorted blocks:\n{blocks}')
-
-for block in blocks:
-    # print(f'\nBefor: {block}')
-    move_down(block, grid)
-    # print(f'After: {block}')
-    # print(f'Grid: {grid}')
+move_all_down(grid, blocks)
+print_blocks(blocks, height=10 if aoc.is_test_mode() else 160)
 
 grid_set = set([tuple(x) for x in grid])
-# grid = list(grid_set)
-# grid = [[*x] for x in grid]
 if len(grid) != len(grid_set):
     raise RuntimeError(f'list {len(grid)} set {len(grid_set)}')
 
 # print(f'Grid:\n{grid}')
 
-removable_blocks = 0
-grid_blocks = len(grid)
-for n, block in enumerate(blocks):
-    print(f'{n+1}: {block}')
-    # tmp_grid = grid #copy.deepcopy(grid)
-    for b in block:
-        # print(f'rem {b}')
-        grid.remove(b)
-    if len(grid) != grid_blocks - len(block):
-        raise RuntimeError(f'{len(grid)} {grid_blocks} {len(block)}')
-    
-    removable = True
-    for other_block in blocks:
-        if other_block is block:
-            # print(f'Removed block, skip')
+# Part 2
+
+for block in blocks:
+    # Sort segments in block from lowest Z to highest
+    block[:] = sorted(block, key=lambda x: x[Z])
+# Sort blocks from highest Z of first segment to lowest
+blocks = sorted(blocks, key=lambda x:x[0][Z], reverse=True)
+print(f'Reverse sorted blocks after fall:\n{blocks}')
+
+# From now on, do not rearrange blocks, as we will use their indexes
+
+# Map grid positions to id of block it is a part of
+xyz_to_block = {}
+for i, block in enumerate(blocks):
+    for x, y, z in block:
+        block_key = tuple(map(tuple, block))
+        xyz_to_block[(x, y, z)] = i
+
+support_blocks = []
+for i, block in enumerate(blocks):
+    support_blocks.append([])
+    z = block[0][Z]
+    if z == 1:
+        print(f'{blocks[i]} sits on ground')
+        continue
+    for x, y, _ in block:
+        key = (x, y, z - 1)
+        if key in xyz_to_block and xyz_to_block[key] not in support_blocks[i]:
+            support_blocks[i].append(xyz_to_block[key])
+
+print('\nSupport blocks:')
+for i, v in enumerate(support_blocks):
+    print(f'{i}: {blocks[i]} lays on {v}')
+
+
+fallable_blocks = 0
+num_blocks = len(support_blocks)
+for i, v in enumerate(support_blocks):
+    val = support_blocks[i]
+    # remove n-th block and validate which blocks will fall
+    support_blocks[i] = []
+    print(f'\nRemoved {i}')
+    # blocks_above = support_blocks[num_blocks-i:]
+    removed_blocks = [i]
+    for j in range(i - 1, -1, -1):
+        # print(f'Checking {j}, support {support_blocks[j]}, removed {removed_blocks}')
+        if support_blocks[j] and all(x in removed_blocks for x in support_blocks[j]):
+            # print(f'Block {j} {blocks[j]} no longer supported')
+            removed_blocks.append(j)
+            fallable_blocks += 1
             continue
-        # print(f'Comparing: {block} vs {other_block}')
-        # print(f'Remaining grid:\n{tmp_grid}')
-        # tmp_grid_2 = copy.deepcopy(grid)
-        for bb in other_block:
-            # print(f'rem {b}')
-            grid.remove(bb)
-        if movable(other_block, grid):
-            # print(f'{block} -> fall {other_block}')
-            removable = False
-            grid += other_block
-            break
-        grid += other_block
-    if removable:
-        # print(f'Block removable: {block}')
-        removable_blocks += 1
 
-    # restore element
-    grid += block
+    support_blocks[i] = val
 
-result1 = removable_blocks
-aoc.print_result(1, result1, exp1)
+result2 = fallable_blocks
 
 # Part 2
 aoc.mark_task_start()
-
 
 
 aoc.print_result(2, result2, exp2)
