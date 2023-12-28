09d54b52d426193acc4095dc56668bab0a0c847b
marcus255 <marek255trojan@gmail.com>
2023-12-28 09:44:57 +0100

    Add task 24 part 2 using sympy

--- 2023/task_24.py
+++ 2023/task_24.py
@@ -1,8 +1,9 @@
 import aoc
+import sympy
 
 lines = aoc.get_lines(__file__)
 result1, result2 = 0, 0
-exp1, exp2 = 2, 1
+exp1, exp2 = 2, 47
 
 # Common
 X, Y = 0, 1
@@ -21,7 +22,7 @@ for line in lines:
     dx, dy, dz = dxdydz.split(', ')
     x, y, z = int(x), int(y), int(z)
     dx, dy, dz = int(dx), int(dy), int(dz)
-    vectors.append(((x, y), (x + dx, y + dy), (dx, dy)))
+    vectors.append(((x, y, z), (x + dx, y + dy, z + dz), (dx, dy, dz)))
 
 def intersection(A, B, C, D):
     # Line AB represented as a1x + b1y = c1
@@ -51,7 +52,8 @@ def is_in_past(v, p):
 # Part 1
 aoc.mark_task_start()
 collides = 0
-t1, t2 = (7, 27) if aoc.is_test_mode() else (200000000000000, 400000000000000)
+MIN, MAX = 200000000000000, 400000000000000
+t1, t2 = (7, 27) if aoc.is_test_mode() else (MIN, MAX)
 for i, v1 in enumerate(vectors):
     for v2 in vectors[i+1:]:
         p = intersection(v1[0], v1[1], v2[0], v2[1])
@@ -65,14 +67,21 @@ for i, v1 in enumerate(vectors):
         
 result1 = collides
 aoc.print_result(1, result1, exp1)
-'''
-19, 13, 30 @ -2,  1, -2
-20, 19, 15 @  1, -5, -3
- (21.44, 11.78)
-'''
+
 # Part 2
 aoc.mark_task_start()
-
-
+x, y, z, dx, dy, dz = sympy.symbols("x, y, z, dx, dy, dz")
+equations = []
+for (xi, yi, zi), _, (dxi, dyi, dzi) in vectors:
+    equations.append((dx - dxi) * (yi - y) - (xi - x) * (dy - dyi))
+    equations.append((dy - dyi) * (zi - z) - (yi - y) * (dz - dzi))
+    solutions = []
+    for solution in sympy.solve(equations):
+        if all(s % 1 == 0 for s in solution.values()) and len(solution) == 6:
+            solutions.append(solution)
+    if len(solutions) == 1:
+        sl = solutions[0]
+        result2 = solutions[0][x] + solutions[0][y] + solutions[0][z]
+        break
 
 aoc.print_result(2, result2, exp2)
