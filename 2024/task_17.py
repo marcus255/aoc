e7826c0113d2612ddcfc0938b0491dea40195653
marcus255 <marek255trojan@gmail.com>
2024-12-17 13:51:50 +0100

    2024 Task 17 part 2

--- 2024/task_17.py
+++ 2024/task_17.py
@@ -2,7 +2,7 @@ import aoc
 
 lines = aoc.get_lines(__file__)
 result1, result2 = 0, 0
-exp1, exp2 = 1, 1
+exp1, exp2 = None, 117440
 
 # Common
 programs_registers = []
@@ -23,34 +23,15 @@ for line in lines:
         programs_registers.append(registers)
         registers = {}
 
-def adv(x):
-    registers['A'] = registers['A'] // 2**x
-def bxl(x):
-    registers['B'] = registers['B'] ^ x
-def bst(x):
-    registers['B'] = x % 8
-def jnz(x):
-    if registers['A'] != 0:
-        global instr_pointer
-        instr_pointer = x - 2
-def bxc(x):
-    registers['B'] = registers['B'] ^ registers['C']
-def out(x):
-    output.append(x % 8)
-def bdv(x):
-    registers['B'] = registers['A'] // 2**x
-def cdv(x):
-    registers['C'] = registers['A'] // 2**x
-
 instructions = {
-    '0': lambda x: adv(x),
-    '1': lambda x: bxl(x),
-    '2': lambda x: bst(x),
-    '3': lambda x: jnz(x),
-    '4': lambda x: bxc(x),
-    '5': lambda x: out(x),
-    '6': lambda x: bdv(x),
-    '7': lambda x: cdv(x),
+    '0': lambda x: registers.update({'A': registers['A'] // 2**x}),
+    '1': lambda x: registers.update({'B': registers['B'] ^ x}),
+    '2': lambda x: registers.update({'B': x % 8}),
+    '3': lambda x: (globals().update({'instr_pointer': x - 2}) if registers['A'] != 0 else None),
+    '4': lambda x: registers.update({'B': registers['B'] ^ registers['C']}),
+    '5': lambda x: output.append(x % 8),
+    '6': lambda x: registers.update({'B': registers['A'] // 2**x}),
+    '7': lambda x: registers.update({'C': registers['A'] // 2**x}),
 }
 combo_operands = {
     '4': lambda: registers['A'],
@@ -58,8 +39,52 @@ combo_operands = {
     '6': lambda: registers['C'],
 }
 
+def run(program):
+    global output
+    global instr_pointer
+    output = []
+    instr_pointer = 0
+    while instr_pointer < len(program) - 1:
+        instr = program[instr_pointer]
+        operand = program[instr_pointer+1]
+        if instr not in ['1', '3'] and 7 > int(operand) >= 4:
+            operand = combo_operands[operand]()
+        instructions[instr](int(operand))
+        instr_pointer += 2
+    return ','.join(map(str, output))
+
+def run2(program, instr_index, match, result):
+    if instr_index < 0:
+        result.append(match)
+        return True
+    for bit_mask in range(8):
+        reg_a, instr_pointer = match << 3 | bit_mask, 0
+        while instr_pointer < len(program) - 1:
+            instr = int(program[instr_pointer])
+            operand = int(program[instr_pointer+1])
+
+            # Define operations in place, globals make looping harder
+            if operand <= 3: op = operand
+            elif operand == 4: op = reg_a
+            elif operand == 5: op = reg_b
+            elif operand == 6: op = reg_c
+
+            if   instr == 0: reg_a //= 2**op
+            elif instr == 1: reg_b ^= operand
+            elif instr == 2: reg_b = op & 7
+            elif instr == 3: instr_pointer = operand - 2 if reg_a != 0 else instr_pointer
+            elif instr == 4: reg_b ^= reg_c
+            elif instr == 5: output_char = op & 7; break
+            elif instr == 6: reg_b = reg_a // 2**op
+            elif instr == 7: reg_c = reg_a // 2**op
+            instr_pointer += 2
+        if output_char == int(program[instr_index]) and run2(program, instr_index - 1, match << 3 | bit_mask, result):
+            return True
+    return False
+
 # Part 1
 results1 = [
+    (0, 0, 0, '5,7,3,0') ,
     (0, 0, 0, '4,6,3,5,6,3,5,2,1,0'),
     (0, 1, 9, ''),
     (10, 0, 0, '0,1,2'),
@@ -69,21 +94,16 @@ results1 = [
 ]
 for i, program in enumerate(programs):
     aoc.mark_task_start()
-    output = []
-    instr_pointer = 0
     registers = programs_registers[i]
-    while instr_pointer < len(program) - 1:
-        instr = program[instr_pointer]
-        operand = program[instr_pointer+1]
-        orig_operand = operand
-        if instr not in ['1', '3'] and 7 > int(operand) >= 4:
-            operand = combo_operands[operand]()
-        instructions[instr](int(operand))
-        instr_pointer += 2
-    result = (registers['A'], registers['B'], registers['C'], ','.join(map(str, output)))
+    output_str = run(program)
+    result = (registers['A'], registers['B'], registers['C'], output_str)
     aoc.print_result(1, result, results1[i])
 
 # Part 2
 aoc.mark_task_start()
-
+program = programs[0]
+registers = programs_registers[0]
+result = []
+run2(program, len(program) - 1, 0, result)
+result2 = result[0]
 aoc.print_result(2, result2, exp2)
