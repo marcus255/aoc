07250ad36798573121ff168f90f8144850ad9db6
Marek Trojan <marek255trojan@gmail.com>
2025-01-09 19:15:11 +0100

    2021 task 3 part 2

--- 2021/task_03.py
+++ 2021/task_03.py
@@ -2,12 +2,22 @@ import aoc
 
 lines = aoc.get_lines(__file__)
 result1, result2 = 0, 0
-exp1, exp2 = 198, 1
+exp1, exp2 = 198, 230
 
 # Common
 numbers = []
 for line in lines:
     numbers.append(list(line))
+
+def find_num(nums, most=True):
+    c = 0
+    while len(nums) > 1:
+        ones = [nums[r][c] for r in range(len(nums))].count('1')
+        zeros = len(nums) - ones
+        condition = zeros > ones if most else ones >= zeros
+        nums = [n for n in nums if n[c] == ('0' if condition else '1')]
+        c += 1
+    return int(''.join(nums[0]), base=2)
 
 # Part 1
 aoc.mark_task_start()
@@ -15,16 +25,12 @@ least, most = '', ''
 for v_num in zip(*numbers):
     ones = v_num.count('1')
     zeros = len(numbers) - ones
-    
     most += '1' if ones > zeros else '0'
     least += '0' if ones > zeros else '1'
-
 result1 = int(most, base=2) * int(least, base=2)
 aoc.print_result(1, result1, exp1)
 
 # Part 2
 aoc.mark_task_start()
-
-
-
+result2 = find_num(numbers[:]) * find_num(numbers[:], most=False)
 aoc.print_result(2, result2, exp2)
